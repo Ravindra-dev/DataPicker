f8c31dbc24fb18b2271a277de3e8d7f2090344ed
unknown <raviambati44@gmail.com>
2020-12-25 11:40:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DatePicker from "./component/DatePicker";
+
+function onChange(timestamp) {
+  console.log(timestamp);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DatePicker onChange={onChange} />
     </div>
   );
 }
